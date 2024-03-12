c5e14cfc9d160d68dbbcc8571ff699b9849e97c9
yasindavids <yasindavids1134@gmail.com>
2024-03-12 12:32:25 +0200

    Changed the look
    Edited the css files to change the look of the look of the application.

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,12 +1,44 @@
+import { useState } from "react"
 import "./styles.css"
 
+function handleSubmit(e){
+  e.preventDefault()
+}
+
 export default function App(){
-
-  return <form className = "new-item-form">
+  const[newItem, setNewItem] = useState("")
+  return ( 
+  <>
+  <form onSubmit={handleSubmit} className = "new-item-form">
     <div className = "form row">
-      <label htmlFor = "item"> New Item</label>
-      <input type = "text" id = "item"></input>
+      <label htmlFor = "item">New Item: </label>
+      <input 
+        value = {newItem}
+        onChange={ e => setNewItem(e.target.value)} 
+        type="text" 
+        id="item"
+      />
     </div>
-
+    <button className = "btn">Add</button>
   </form>
+  <h1 className="header">To Do List</h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 1
+      </label>
+      <button className="btn btn-danger"> delete </button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 2
+      </label>
+      <button className="btn btn-danger"> delete </button>
+    </li>
+
+  </ul>
+  </>
+  )
 }
