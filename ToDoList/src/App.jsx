06527b5d6ff39a994001f68c47afca8b3d2db85e
yasindavids <yasindavids1134@gmail.com>
2024-03-12 18:54:36 +0200

    Update App
    Added a lot of basic functionality, such as adding tasks, toggling tasks, and deleting tasks

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,44 +1,90 @@
-import { useState } from "react"
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
 
-function handleSubmit(e){
-  e.preventDefault()
-}
+export default function App() {
+  {
+    //States
+  }
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
 
-export default function App(){
-  const[newItem, setNewItem] = useState("")
-  return ( 
-  <>
-  <form onSubmit={handleSubmit} className = "new-item-form">
-    <div className = "form row">
-      <label htmlFor = "item">New Item: </label>
-      <input 
-        value = {newItem}
-        onChange={ e => setNewItem(e.target.value)} 
-        type="text" 
-        id="item"
-      />
-    </div>
-    <button className = "btn">Add</button>
-  </form>
-  <h1 className="header">To Do List</h1>
-  <ul className="list">
-    <li>
-      <label>
-        <input type="checkbox"/>
-        Item 1
-      </label>
-      <button className="btn btn-danger"> delete </button>
-    </li>
-    <li>
-      <label>
-        <input type="checkbox"/>
-        Item 2
-      </label>
-      <button className="btn btn-danger"> delete </button>
-    </li>
+  //Function to add button functionality
+  function handleSubmit(e) {
+    e.preventDefault();
 
-  </ul>
-  </>
-  )
+    //Array to store new tasks
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+    setNewItem("");
+  }
+
+  //Toggles todo tasked based on whether its been checked or not
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id == id) {
+          return { ...todo, completed }; //Creates new state object
+        }
+        return todo;
+      });
+    });
+  }
+
+  //Deletes tasks
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id); //Returns all tasks except the checked one
+    });
+  }
+  /*---------------------------------------------------------------------------------------------------------- */
+  return (
+    <>
+      {/*Manages button and item inputs*/}
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form row">
+          <label htmlFor="item">New Task: </label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)} //Sets input to new item
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      {/*List of checkboxes for task*/}
+      <h1 className="header">To Do List</h1>
+      <ul className="list">
+        {todos.length === 0 && "Nothing To Do!"}{" "}
+        {/*short circuit to print msg*/}
+        {/**Returns an <li> element for each todo element */}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)} //Calls toggle function to check the box
+                />
+
+                {todo.title}
+              </label>
+              <button
+                onClick={() => deleteTodo(todo.id)} //Calls deleteTodo on click
+                className="btn btn-danger"
+              >
+                {" "}
+                delete{" "}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
